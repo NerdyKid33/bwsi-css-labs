0cb77d6439415c6259c8db052cbe5232372ca70a
NerdyKid33 <aarav.gupta.201201@gmail.com>
2026-02-10 22:44:42 -0500

    added test print statement lab 1a

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Aarav Gupta" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("hello i am printing a test")
 
 if __name__ == "__main__":
     main()
